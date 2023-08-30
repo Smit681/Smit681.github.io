e6a8a425d7d37a3898da18c92a0d322470230afc
Smit681 <76237944+Smit681@users.noreply.github.com>
2023-08-30 09:11:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -28,54 +28,95 @@ function Width() {
     );
   }
 
+const top_right = document.getElementsByClassName("top-right")[0];
+const name1 = document.getElementsByClassName('name')[0];
+const position = document.getElementsByClassName('position')[0];
+const blackSection = document.getElementById("project");
+
 window.addEventListener("scroll", (event) => {
-    let scrollY = this.scrollY;
-    console.log(scrollY);
-    console.log(Width());
-    if(Width() > 760){
-        if(scrollY >= 912 && scrollY <= 1648){
-            document.getElementsByClassName('name')[0].style.color = "white";
-            document.getElementsByClassName('position')[0].style.color = "white";
-            document.getElementsByClassName('top-right')[0].style.color = "white";
-        }
-        else{
-            document.getElementsByClassName('name')[0].style.color = "#0f141e";
-            document.getElementsByClassName('position')[0].style.color = "#0f141e";
-            document.getElementsByClassName('top-right')[0].style.color = "#0f141e";    
-        }
+    const fixedTextRect = top_right.getBoundingClientRect();
+    const nameRect = name1.getBoundingClientRect();
+    const positionRect = position.getBoundingClientRect();
+
+    const blackSectionRect = blackSection.getBoundingClientRect();
+
+    const isOverlapping = (
+        fixedTextRect.bottom > blackSectionRect.top &&
+        fixedTextRect.top < blackSectionRect.bottom
+    );
+
+     // Change the text color based on overlap
+     if (isOverlapping) {
+        top_right.style.color = "white";
+    } else {
+        top_right.style.color = "black";
     }
-    else {
-        if(scrollY >= 780 && scrollY <= 1266){  
-            document.getElementsByClassName('name')[0].style.color = "white";
-            document.getElementsByClassName('position')[0].style.color = "white";
-            document.getElementsByClassName('top-right')[0].style.color = "white";
-        }
-        else {
-            document.getElementsByClassName('name')[0].style.color = "#0f141e";
-            document.getElementsByClassName('position')[0].style.color = "#0f141e";
-            document.getElementsByClassName('top-right')[0].style.color = "#0f141e";
-        }
+
+    const isOverlapping2 = (
+        nameRect.bottom > blackSectionRect.top &&
+        nameRect.top < blackSectionRect.bottom &&
+        positionRect.bottom > blackSectionRect.top &&
+        positionRect.top < blackSectionRect.bottom
+    );
+
+     // Change the text color based on overlap
+     if (isOverlapping2) {
+        name1.style.color = "white";
+        position.style.color = "white";
+
+    } else {
+        name1.style.color = "black";
+        position.style.color = "black";
 
     }
-   
-     
 
-    if(Width() > 760){
-        if(scrollY >= 188 && scrollY <= 1000){
-            document.getElementsByClassName('bottom-right')[0].style.color = "white";
-        }
-        else{
-            document.getElementsByClassName('bottom-right')[0].style.color = "#0f141e";
-        }
-    }
-    else{
-        if(scrollY >= 188 && scrollY <= 672){
-            document.getElementsByClassName('bottom-right')[0].style.color = "white";
-        }
-        else{
-            document.getElementsByClassName('bottom-right')[0].style.color = "#0f141e";
-        }
-    }
+
+    // let scrollY = this.scrollY;
+    // console.log(scrollY);
+    // console.log(Width());
+    // if(Width() > 760){
+    //     if(scrollY >= 912 && scrollY <= 1648){
+    //         document.getElementsByClassName('position')[0].style.color = "white";
+    //         document.getElementsByClassName('top-right')[0].style.color = "white";
+    //     }
+    //     else{
+    //         document.getElementsByClassName('name')[0].style.color = "#0f141e";
+    //         document.getElementsByClassName('position')[0].style.color = "#0f141e";
+    //         document.getElementsByClassName('top-right')[0].style.color = "#0f141e";    
+    //     }
+    // }
+    // else {
+    //     if(scrollY >= 780 && scrollY <= 1266){  
+    //         document.getElementsByClassName('name')[0].style.color = "white";
+    //         document.getElementsByClassName('position')[0].style.color = "white";
+    //         document.getElementsByClassName('top-right')[0].style.color = "white";
+    //     }
+    //     else {
+    //         document.getElementsByClassName('name')[0].style.color = "#0f141e";
+    //         document.getElementsByClassName('position')[0].style.color = "#0f141e";
+    //         document.getElementsByClassName('top-right')[0].style.color = "#0f141e";
+    //     }
+
+    // }
+
+
+
+    // if(Width() > 760){
+    //     if(scrollY >= 188 && scrollY <= 1000){
+    //         document.getElementsByClassName('bottom-right')[0].style.color = "white";
+    //     }
+    //     else{
+    //         document.getElementsByClassName('bottom-right')[0].style.color = "#0f141e";
+    //     }
+    // }
+    // else{
+    //     if(scrollY >= 188 && scrollY <= 672){
+    //         document.getElementsByClassName('bottom-right')[0].style.color = "white";
+    //     }
+    //     else{
+    //         document.getElementsByClassName('bottom-right')[0].style.color = "#0f141e";
+    //     }
+    // }
 });
 
 function mouseIn() {
@@ -93,7 +134,7 @@ function contact(){
     var cnt = document.getElementsByClassName('contact-me')[0];
     cnt.classList.add("trigger-Annimation");
     cnt.classList.remove("remove-Annimation");
-    
+
     var flip = document.getElementsByClassName('back')[0];
     flip.classList.add("flip");
     flip.classList.remove("flip-back");
